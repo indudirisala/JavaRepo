e206de8810943766cb83d8b44bc1a8dd425b78f6
965657 <965657@01hw853839.India.TCS.com>
2016-01-21 11:19:13 +0530

    vc3

--- Vc_Sample/src/Fibnocci.java
+++ Vc_Sample/src/Fibnocci.java
@@ -8,8 +8,11 @@ public class Fibnocci {
 		int f3;
 		System.out.print(f1+"\t"+f2);
 		
-		for(int i=0;i<20;i++) {
+		for(int i=0;i<10;i++) {
 			f3=f1+f2;
+			f1=f2;
+			f2=f3;
+			
 			System.out.print("\t"+f3);
 		}
 	}
